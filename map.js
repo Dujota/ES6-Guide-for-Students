9aa5eb035fbda86ba29a964d5aa1830f0ec5a9b5
Denis <ddujota@internationalsafety.com>
2018-09-19 09:48:06 -0400

    did a slightly more complex example

--- map.js
+++ map.js
@@ -14,6 +14,21 @@ console.log("numbers", numbers);
 var doubled = numbers.map(number => {
   return number * 2 // always make sure you use the return keyword!!!
 })
-
 // has the same result as the for loop iteration
 console.log("map function:", doubled);
+
+
+// more complicated example of Map: - array of cars objects
+
+var cars = [
+  {model: 'Chevrolet', price: 'cheap'},
+  {model: 'BMW', price: 'expensive'}
+];
+
+// this is a very common practice in front end frameworks for extracting data from a backend response
+var prices = cars.map(car =>{
+  return car.price // referred as a pluck
+})
+console.log(prices);
+
+// The primary goal of most web apps are just rendering lists of data, this is why map is the most popular used array helper because it does not mutate any data.
