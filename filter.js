b9b25b6462c4d57af1b41f61e5e26534f6eebfce
Denis <ddujota@internationalsafety.com>
2018-09-19 10:11:42 -0400

    stater code for ex done

---
+++ filter.js
@@ -0,0 +1,18 @@
+/**
+ * common ui feature is a way to filter/sort of lists - like get a filtered list of products
+ */
+
+ var products = [
+   {name: "glasses", type: "casual"},
+   {name: "button shirt", type: "formal"},
+   {name: "t-shirt", type: "casual"},
+   {name: "suit", type: "formal"}
+ ];
+
+ // use case -- these are products on a product page and maybe the user clicks a button that triggers a filter on that list
+
+ // ES5 Way of how the filter works
+ var filteredProducts = []
+ for (var i = 0; i < products.length; i++) {
+   products[i]
+ }
