d70b5560149fb572090b89c0a9f77791973c5cf6
Denis <ddujota@internationalsafety.com>
2018-09-19 09:12:43 -0400

    file structure created:

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Create an arrray of numbers
+  var numbers = [1, 2, 3, 4, 5];
+
+  // create a variable to hold the sum
+  var sum = 0;
+
+  // loop over the array, incremeneting the sum variable
+
+
+  // print the sum variable
