40a2c9f75df4d50edaa014c17c067018cb93078f
Denis <ddujota@internationalsafety.com>
2018-09-19 11:55:58 -0400

    created the find file and then required it to the html page and started the es5 version of find

---
+++ find.js
@@ -0,0 +1,25 @@
+/**
+ * purpose is to search through the array and look for a particular element and return that record back
+ * CAVEAT -- it only returns the 1st record even if there are other records that may satisfy the find clause
+ */
+
+ var users = [
+   {name: 'john'},
+   {name: 'mary'},
+   {name: 'steve'},
+ ]
+
+ // ES5 way -- set up the user var and then use a for loop to navigate through the array and then re-assign the match ti the initialized variable
+ var user;
+ // issue , we do not break out of the loop when we find the user unless we do a break statement
+ for (var i = 0; i < users.length; i++) {
+   if (users[i].name === 'john'){
+     user = users[i]
+     break;
+   }
+ }
+ console.log(user);
+
+ // ES6 Find helper
+
+
