8e2a1a9c3e47f5ccd674e1ba0ed34704b384e23e
Denis <ddujota@internationalsafety.com>
2018-09-19 09:21:48 -0400

    exMPLE 1

--- forEach/app.js
+++ forEach/app.js
@@ -16,3 +16,16 @@ console.log(sum);
 
 //  When to use forEach helper - whenever we want to call soem method multiuple times passing in a different argument each time, consider using the forEach helper
 
+// EX#1
+
+function handlePosts() {
+  var posts = [
+    { id: 23, title: 'Daily JS News' },
+    { id: 52, title: 'Code Refactor City' },
+    { id: 105, title: 'The Brightest Ruby' }
+  ];
+
+  for (var i = 0; i < posts.length; i++) {
+    savePost(posts[i]);
+  }
+}
